dc95593bb8050ab4954e5988221072ed37638b04
DreamyBoy <dreamyboy@cornoi.de>
2026-07-24 14:13:07 -0600

    test again

--- boombox_tracks.lua
+++ boombox_tracks.lua
@@ -1,4 +1,5 @@
 -- paste me in STREAM.tracks in your script.lua
 tracks = {
+    { id = "hour_hand", title = "HOUR HAND - Remix", segCount = 75, segSeconds = 3 },
     { id = "troubled_plains", title = "Troubled Plains", segCount = 52, segSeconds = 3 },
 }
