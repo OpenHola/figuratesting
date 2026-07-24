e245d9ce8edcbe9dbc8762d4671471ac10503613
DreamyBoy <dreamyboy@cornoi.de>
2026-07-24 15:39:04 -0600

    AGAIN

--- boombox_tracks.lua
+++ boombox_tracks.lua
@@ -1,5 +1,5 @@
 -- paste me in STREAM.tracks in your script.lua
 tracks = {
-    { id = "hour_hand", title = "HOUR HAND - Remix", segCount = 2, segSeconds = 120, bytes = 304632, segSizes = {164280, 140352} },
+    { id = "hour_hand", title = "HOUR HAND - Remix", segCount = 15, segSeconds = 15, bytes = 335407, segSizes = {22144, 22382, 23185, 23152, 22085, 22152, 22879, 22868, 22308, 21997, 22981, 23097, 22765, 21634, 19778} },
     { id = "troubled_plains", title = "Troubled Plains", segCount = 11, segSeconds = 15, bytes = 222512, segSizes = {21292, 21258, 21247, 22034, 21993, 22499, 21209, 20976, 22130, 21873, 6001} },
 }
